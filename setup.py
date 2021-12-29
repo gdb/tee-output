71c79754a9349b326a0f7881407b21d5ff8ecfc2
Greg Brockman <gdb@gregbrockman.com>
2021-12-29 12:26:42 -0800

    Fix parent-lifetime

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tee-output',
-      version='0.1',
+      version='0.2',
       description='A utility to tee standard output / standard error from the current process into a logfile. Preserves terminal semantics, so breakpoint() etc continue to work.',
       author='Greg Brockman',
       packages=['tee_output'],
